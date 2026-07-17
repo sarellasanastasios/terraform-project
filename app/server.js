b507670ec7c1effcbc5b54e059b7fab100e05639
Sarellas Anastasios <sarellasanastasios@hotmail.com>
2026-07-17 19:21:31 +0300

    Add health/ready endpoints,update Dockerfile,update deployment to v3

--- app/server.js
+++ app/server.js
@@ -5,12 +5,20 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello from AKS Node.js!" });    
 });
 
-app.get("health/", (req, res) => {
-    res.json({ status: "ok" });
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ status: "ok" });
 });
+
+
+app.get("/ready", (req, res) => {
+    res.status(200).json({ status: "ready" });
+});
+
+
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('Server running on port ${port}');
+    console.log(`Server running on port ${port}`);
 });
